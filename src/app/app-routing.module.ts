78d4e4ec210a00f93ec407f06621917b6f168abb
insaneAlex <syvoplias76@gmail.com>
2019-09-09 18:38:00 +0300

    Login, Register pages done, commented add module imports, router toolbar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
-import {LandingPageComponent} from './components/landing-page/landing-page.component';
-import {LoginComponent} from './components/login/login.component';
-import {MyAccountComponent} from './components/my-account/my-account.component';
-import {SearchPageComponent} from './components/search-page/search-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'my-account', component: MyAccountComponent },
   { path: 'search', component: SearchPageComponent },
   { path: '**', component: PageNotFoundComponent }
--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -1,19 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { LoginComponent } from './login.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+import { RegisterComponent } from './register.component';
+
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [ RegisterComponent ]
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
