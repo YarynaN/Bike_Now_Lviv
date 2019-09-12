12b32d0a148e9afad207647538a46c21d3c0c78a
Nataliya Kuziv <neti.kuzja@gmail.com>
2019-09-12 08:53:28 +0300

    Added nav-menu for My account page

---
+++ src/app/components/my-account/bikes-info/bikes-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bikes-info',
+  templateUrl: './bikes-info.component.html',
+  styleUrls: ['./bikes-info.component.scss']
+})
+export class BikesInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-account',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyAccountComponent implements OnInit {
 
-  constructor() { }
+  dataUser = {
+    name: 'Natalja',
+    surname: 'Kuziv',
+    photo: 'https://cdn.pixabay.com/photo/2016/10/10/14/13/dog-1728494_1280.png',
+    email: 'lucky@gmail.com',
+    phone: '+380359874638',
+    birthday: '29/08/2009',
+    height: '165 cm',
+    myDate_button: 'MY DATA',
+    myBike_button: 'MY BIKE',
+    goBack_button: 'go back'
+  };
+
+  myBike = [
+    {
+      brand: 'CUBE',
+      model: 'Access WLS Race',
+      category: 'Mountain bikes',
+      size: '17" S (S - height 160 - 168 cm)',
+      color: 'black-blue-green',
+      weight: '14 kg',
+      frame: 'aluminium',
+      speeds: '30',
+      brakes: 'disk hydraulic',
+      wheels_diameter: '29"'
+    }
+  ];
+
+  constructor(
+    private location: Location,
+    private router: Router
+  ) { }
+
+  
 
   ngOnInit() {
   }
 
+  secondaryNav(path) {
+    this.router.navigate([{ outlets: {
+      sidebar: [path]
+    }}]);
+  }
+
 }
+
+// @Component({
+//   selector: 'divider-overview-example',
+//   templateUrl: 'divider-overview-example.html',
+//   styleUrls: ['divider-overview-example.css'],
+// })
+// export class DividerOverviewExample {}
