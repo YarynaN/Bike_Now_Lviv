84a09553b8fa05d4747ed43eeb51e71fdc24d0ce
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-09-29 18:21:06 +0300

    multiple bikes working

--- src/app/services/bike-info.service.ts
+++ src/app/services/bike-info.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { BikeInfo } from '../models/bike-info.model';
 import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
-import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import * as firebase from 'firebase';
+
 @Injectable({
   providedIn: 'root'
 })
 export class BikeInfoService {
   bikesData: AngularFireList<any>;
   db: AngularFireDatabase;
-  constructor(db: AngularFireDatabase) {
+  constructor(db: AngularFireDatabase ) {
     this.db = db;
     this.bikesData = db.list('/usersData');
   }
 
-  updateUserBikeItem( key: string,  model: string  , brand: string , categories: string , sizes: string ,
+  updateUserBikeItem( userId: string, bikeId: string,  model: string  , brand: string , categories: string , sizes: string ,
                       color: string , weight: string , frames: string , speeds: string , brakes: string , diameterWheels: string ) {
     const bike = {
       bike_model: model,
@@ -29,19 +29,16 @@ export class BikeInfoService {
       bike_brakes : brakes,
       bike_Wheels : diameterWheels
     };
-    const newBikeKey = firebase.database().ref().child('/bikes').push().key;
-    const dynamicProperty = newBikeKey;
-    this.bikesData.update(key, { bikes : {   [dynamicProperty] : bike    }});
+    let key;
+    key = bikeId + '/bikes/';
+    this.bikesData.update(key, { [bikeId] : bike});
   }
   pushUserBikeItem( key: string,  model: string  , brand: string , categories: string , sizes: string ,
                     color: string , weight: string , frames: string , speeds: string , brakes: string , diameterWheels: string ) {
-
-    const newBikeKey = firebase.database().ref().child('/bikes').push().key;
-    const dynamicProperty = newBikeKey;
     const bike = {
       bike_model: model,
       bike_brand: brand,
-      bike_categorie: categories,
+      bike_category: categories,
       bike_size  : sizes,
       bike_color : color,
       bike_weight : weight,
@@ -50,18 +47,17 @@ export class BikeInfoService {
       bike_brakes : brakes,
       bike_Wheels : diameterWheels
     };
-
-    this.bikesData.update(key, { bikes : {   [dynamicProperty] : bike    }});
+    const newBikeKey = firebase.database().ref().child('/bikes/').push().key;
+    const dynamicProperty = newBikeKey;
+    key = key + '/bikes/' + dynamicProperty + '/' ;
+    this.bikesData.update(key, { [dynamicProperty] : bike});
   }
 
-  getBikesById(idUser: string, idBike) {
-    const obj = this.db.object('/usersData/' + idUser + '/bikes/' + idBike + '/').snapshotChanges().pipe(
+  getBikesById(idUser: string, idBike: string) {
+    const obj = this.db.object('/usersData/' + idUser + '/bikes/' + idBike + '/'  ).snapshotChanges().pipe(
       map(o => ({id: o.payload.key, ...o.payload.val()}))
     );
     return obj;
   }
 
-  save(data: BikeInfo): boolean {
-    return true;
-  }
 }
