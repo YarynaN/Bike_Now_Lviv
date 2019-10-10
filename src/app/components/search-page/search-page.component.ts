d10230624dde228443ce947b1ccf2243679fa23b
insaneAlex <syvoplias76@gmail.com>
2019-10-10 21:18:11 +0300

    update

--- src/app/components/search-page/search-page.component.ts
+++ src/app/components/search-page/search-page.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import * as algoliasearch from 'algoliasearch/lite';
+
+const searchClient = algoliasearch(
+  'L0RIMQ3N9M',
+  'd46d47318500059ef7e4a90ece2b341b'
+);
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.scss']
 })
-export class SearchPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SearchPageComponent {
+  config = {
+    indexName: 'bicycles',
+    searchClient,
+    routing: true
   }
-
 }
 
